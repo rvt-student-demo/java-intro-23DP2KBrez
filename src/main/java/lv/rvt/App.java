02ff5d57db0d4400f9b88975e899e7d400d1ec30
KIRILLS BREŽŅEVS <140741714+23DP2KBrez@users.noreply.github.com>
2024-09-05 10:07:54 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,26 @@
 package lv.rvt;
+import java.util.Scanner;
 
+//First realisation (without OOP)
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println("Hello RVT!");
-    }
+    {   
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        String surname;
+        String group;
+        
+        System.out.println("Enter your name:");
+        name = scanner.nextLine();
+        System.out.println("Enter your surname:");
+        surname = scanner.nextLine();
+        System.out.println("Enter your group:");
+        group = scanner.nextLine();
+
+        System.out.println("You aswers:");
+        System.out.println(name+" "+surname+" "+group);
+
+        scanner.close();
+    } 
 }

12b2db1d4f2b9254a4ef479ff1736062fb9ee9a1
KIRILLS BREŽŅEVS <140741714+23DP2KBrez@users.noreply.github.com>
2024-09-11 09:33:50 +0000

    Uzdevums 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,16 +1,49 @@
 package lv.rvt;
-import java.util.Scanner;
+import java.util.*;
+
+class InputManager
+{
+    Scanner scanner;
+
+    InputManager(Scanner scanner)
+    {
+        this.scanner = scanner;
+    }
+
+    public int getInt()
+    {
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public float getFloat()
+    {
+        return Float.parseFloat(scanner.nextLine());
+    }
+
+    public String getString()
+    {
+        return scanner.nextLine();
+    }
+
+}
 
 public class App 
 {
+    static InputManager inputManager;
     public static void main( String[] args )
     {
-        int chicken = 3;   
-        double bacon = 5.5; 
-        String tractor = "None!";
-        System.out.println("And finally, a summary:");
-        System.out.println(chicken);
-        System.out.println(bacon);
-        System.out.println(tractor);
+        Scanner scanner = new Scanner(System.in);
+        inputManager = new InputManager(scanner);
+        
+        System.out.println("Ievadi savu vardu:");
+        String name = inputManager.getString();
+        System.out.println("Ievadi savu uzvardu:");
+        String surname = inputManager.getString();
+        System.out.println("Ievadi savu grupu:");
+        String group = inputManager.getString();
+
+        System.out.println("Skolnieks: "+name+" "+surname+"; Grupa: "+group);
+
+        scanner.close();
     }
 }

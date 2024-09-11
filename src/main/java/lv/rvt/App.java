48f45cb60c447eef5b279332a9cbaa1b5a0aa696
KIRILLS BREŽŅEVS <140741714+23DP2KBrez@users.noreply.github.com>
2024-09-11 08:11:20 +0000

    Various Variables

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,13 +1,12 @@
 package lv.rvt;
 import java.util.Scanner;
 
-//First realisation (without OOP)
 public class App 
 {
     public static void main( String[] args )
     {
         int chicken = 3;   
-        double bacon = 5.5;
+        double bacon = 5.5; 
         String tractor = "None!";
         System.out.println("And finally, a summary:");
         System.out.println(chicken);

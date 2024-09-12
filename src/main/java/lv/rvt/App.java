5317397fce1d23393949278b5f4e8e94da323977
KIRILLS BREŽŅEVS <140741714+23DP2KBrez@users.noreply.github.com>
2024-09-12 09:16:29 +0000

    Uzdevums 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,19 +12,18 @@ class InputManager
 
     public int getInt()
     {
-        return Integer.parseInt(scanner.nextLine());
+        return Integer.valueOf(scanner.nextLine());
     }
 
     public float getFloat()
     {
-        return Float.parseFloat(scanner.nextLine());
+        return Float.valueOf(scanner.nextLine());
     }
 
     public String getString()
     {
         return scanner.nextLine();
     }
-
 }
 
 public class App 
@@ -34,16 +33,10 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         inputManager = new InputManager(scanner);
-        
-        System.out.println("Ievadi savu vardu:");
-        String name = inputManager.getString();
-        System.out.println("Ievadi savu uzvardu:");
-        String surname = inputManager.getString();
-        System.out.println("Ievadi savu grupu:");
-        String group = inputManager.getString();
 
-        System.out.println("Skolnieks: "+name+" "+surname+"; Grupa: "+group);
-
-        scanner.close();
+        float first = inputManager.getFloat();
+        float second = inputManager.getFloat();
+    
+        System.out.println(first + second);
     }
 }

f0634523caae039fe61d24ec7571a132191d60e2
KIRILLS BREŽŅEVS <140741714+23DP2KBrez@users.noreply.github.com>
2024-09-18 08:20:09 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -29,17 +29,45 @@ class InputManager
 public class App 
 {
     static InputManager inputManager;
+    static Scanner scanner;
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         inputManager = new InputManager(scanner);
 
-        float first = inputManager.getFloat();
-        float second = inputManager.getFloat();
-    
-        System.out.println(first + " + " + second + " = " + (first + second));
-        System.out.println(first + " - " + second + " = " + (first - second));
-        System.out.println(first + " * " + second + " = " + (first * second));
-        System.out.println(first + " / " + second + " = " + (first / second));
+        System.out.println("Give points[0-100]:");
+        float points = inputManager.getFloat();
+        if(points < 0)
+        {
+            System.out.println("Impossible");
+        }
+        else if (points < 49 && points > 0)
+        {
+            System.out.println("failed");
+        }
+        else if (points > 49 && points < 60)
+        {
+            System.out.println("1");
+        }
+        else if (points >= 60 && points <= 70)
+        {
+            System.out.println("2");
+        }
+        else if (points >= 70 && points <= 80)
+        {
+            System.out.println("3");
+        }
+        else if (points >= 80 && points <= 90)
+        {
+            System.out.println("4");
+        }
+        else if (points >= 90 && points <= 100)
+        {
+            System.out.println("5");
+        }
+        else if (points > 100)
+        {
+            System.out.println("Incredible!");
+        }
     }
 }

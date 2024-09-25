4af4d92f6da275ea4470b9f62f1584e8f3d6c6cb
KIRILLS BREŽŅEVS <140741714+23DP2KBrez@users.noreply.github.com>
2024-09-25 09:32:39 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -35,31 +35,23 @@ public class App
         scanner = new Scanner(System.in);
         inputManager = new InputManager(scanner);
 
-        System.out.println("Value of the gift?");
-        int value = inputManager.getInt();
-        if (value < 5000)
+        while (true) 
         {
-            System.out.println("No tax!");
-        }
-        else if (value >= 5000 && value < 25000)
-        {
-            System.out.println("Tax: " + (100 + (value-5000)*0.08));
-        }
-        else if (value >= 25000 && value < 55000)
-        {
-            System.out.println("Tax: " + (1700 + (value-25000)*0.1));
-        }
-        else if (value >= 55000 && value < 200000)
-        {
-            System.out.println("Tax: " + (4700 + (value-55000 )*0.12));
-        }
-        else if (value >= 200000 && value < 1000000)
-        {
-            System.out.println("Tax: " + (22100 + (value-200000)*0.15));
-        }
-        else if (value >= 1000000)
-        {
-            System.out.println("Tax: " + (142100 + (value-1000000)*0.17));
+            System.out.println("Give a number: ");
+            int number = inputManager.getInt();
+            if (number < 0) 
+            {
+                System.out.println("Unsuitable number: ");
+                continue;
+            }
+            else if (number == 0) 
+            {
+                break;
+            }
+            else
+            {
+                System.out.println(number*number);
+            }
         }
         scanner.close();
     }

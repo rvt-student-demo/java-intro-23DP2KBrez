f4713bc503442a0264be561d23e7204b9f264558
KIRILLS BREŽŅEVS <140741714+23DP2KBrez@users.noreply.github.com>
2024-10-10 08:09:38 +0000

    Advanced astrology

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -37,35 +37,11 @@ public class App
         scanner = new Scanner(System.in);
         inputManager = new InputManager(scanner);
 
-        float number = 0;
-        float sum = 0;
-        int count = 0;
-        int odd = 0;
-        int even = 0;
-        while(true)
-        {
-            number = inputManager.getFloat();
-            sum += number;
-            if(number < 0)
-            {
-                break;
-            }
-            if(number % 2 == 0)
-            {
-                even++;
-            }
-            else
-            {
-                odd++;
-            }
-
-            count++;
-        }
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + count);
-        System.out.println("Average: " + (sum/count));
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
+        Stars.printSquare(3);
+        Stars.printRectangle(17, 3);
+        Stars.printTriangle(4);
+        Stars.printRightTriangle(4);
+        Stars.christmasTree(4);
+        Stars.christmasTree(10);
     }
 }
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -11,6 +11,14 @@ public class Stars
         System.out.println();
     }
 
+    public static void printSpaces(int spaces)
+    {
+        for(int i = 0; i < spaces; i++)
+        {
+            System.out.print(" ");
+        }
+    }
+
     public static void printSquare(int size)
     {
         for(int i = 0; i < size; i++)
@@ -32,6 +40,31 @@ public class Stars
         for(int i = 1; i < size + 1; i++)
         {
             printStars(i);
+        }
+    }
+
+    public static void printRightTriangle(int size)
+    {
+        for(int i = 1; i < size + 1; i++)
+        {
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height)
+    {
+        int stars = 1;
+        for(int i = 1; i < height + 1; i++)
+        {
+            printSpaces(height - i);
+            printStars(stars);
+            stars += 2;
+        }
+        for(int i = 0; i < 3; i++)
+        {
+            printSpaces(height - 2);
+            printStars(3);
         }
     }
 }

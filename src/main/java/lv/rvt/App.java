9e25db15f6824bd72f566839e26eccf7717757fa
KIRILLS BREŽŅEVS <140741714+23DP2KBrez@users.noreply.github.com>
2024-10-16 09:42:14 +0000

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,52 +1,23 @@
 package lv.rvt;
 import java.util.*;
 
-class InputManager
-{
-    Scanner scanner;
-
-    InputManager(Scanner scanner)
-    {
-        this.scanner = scanner;
-    }
-
-    public int getInt()
-    {
-        return Integer.valueOf(scanner.nextLine());
-    }
-
-    public float getFloat()
-    {
-        return Float.valueOf(scanner.nextLine());
-    }
-
-    public String getString()
-    {
-        return scanner.nextLine();
-    }
-}
-
-
 public class App 
 {
-    static InputManager inputManager;
     static Scanner scanner;
-
-    public static void printText() 
-    {
-        System.out.println("In a hole in the ground there lived a method");
-    }
     public static void main( String[] args )
     {
         scanner = new Scanner(System.in);
-        inputManager = new InputManager(scanner);
+        ArrayList<Integer> list = new ArrayList<>();
 
-        System.out.println("How many times?");
-        int number = inputManager.getInt();
-
-        for(int i = 0; i < number; i++)
+        while(true)
         {
-            printText();
+            int ievade = Integer.parseInt(scanner.nextLine());
+            if(ievade == 0)
+            {
+                System.out.println(list.get(1)+ list.get(2));
+                break;
+            }
+            list.add(ievade);
         }
     }
 }
